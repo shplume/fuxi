cc1c125a424e2fc66491c0930df25acc8bcb1c01
Jundan Zhang <1750132640@qq.com>
2024-04-16 15:06:51 +0800

    Feature: similarities

--- baidu/similarity.py
+++ baidu/similarity.py
@@ -3,9 +3,6 @@ from aip import AipNlp
 with open('key.txt') as f:
     info = f.read().split('\n')
 
-access_key_id = info[0]
-access_key_secret = info[1]
-
 APP_ID = info[2]
 API_KEY = info[3]
 SECRET_KEY = info[4]
